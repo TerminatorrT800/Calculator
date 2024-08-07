f94d9d3e48b87aa80d0700da428b71400866ea1f
TerminatorrT800 <nikonikolic261@gmail.com>
2024-08-08 00:26:37 +0200

    Add dot and delete functionalities

--- logic.js
+++ logic.js
@@ -30,20 +30,63 @@ var operation = null;
 var secondOperand = null;
 var numberAfterOperation = false;
 var isDot = false;
-
 var opIndex = null;
+var trash;
 
 //displayValue = `${firstOperand}  ${operation}  ${secondOperand}`;
 
 displayScreen.innerText = "";
+
+createCalc();
 
 clear.addEventListener('click', () => {
     displayScreen.innerText = "";
     firstOperand = operation = secondOperand = opIndex = null;
     numberAfterOperation = false;
     isDot = false;
-    console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
+    //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
 });
+
+deleteBTN.addEventListener("click", () => {
+    if (displayScreen.innerText !== "") {
+        if(firstOperand !== null && operation !== null){
+            trash = displayScreen.innerText.substring(displayScreen.innerText.length-1);
+            displayScreen.innerText = displayScreen.innerText.substring(0, displayScreen.innerText.length-1);
+           // console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1`);
+            if(trash=="."){
+                isDot=false;
+                //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1.1`);
+            }else if(trash=="+" || trash=="-" || trash=="*" || trash=="/"){
+                opIndex= null;
+                operation =null;
+                numberAfterOperation = true;
+                //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1.2`);
+                if(!firstOperand.includes(".")){
+                    isDot =false;
+                    console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1.2.1`);
+                }else{
+                    isDot = true;
+                }
+
+            }else if(displayScreen.innerText.endsWith("+") || displayScreen.innerText.endsWith("-") || displayScreen.innerText.endsWith("*") || displayScreen.innerText.endsWith("/")){
+                isDot = true;
+                //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1.3`);
+            }
+        }else{
+            trash = displayScreen.innerText.substring(displayScreen.innerText.length-1);
+            displayScreen.innerText = displayScreen.innerText.substring(0, displayScreen.innerText.length-1);
+            //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 2`);
+        }
+        if(!firstOperand.includes(".")){
+            isDot =false;
+            //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${isDot} 1.2.1`);
+        }else{
+            isDot = true;
+        }
+        
+    }
+
+})
 
 zero.addEventListener("click", () => {
     displayScreen.innerText += zero.textContent;
@@ -51,38 +94,33 @@ zero.addEventListener("click", () => {
 });
 
 equal.addEventListener("click", () => {
-    if (!displayScreen.innerText == "" && numberAfterOperation == true && opIndex == null) {
+    if (displayScreen.innerText !== "" && numberAfterOperation == true && opIndex == null) {
         firstOperand = displayScreen.innerText;
         displayScreen.innerText = firstOperand;
-        console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
-    } else if (!displayScreen.innerText == "" && numberAfterOperation == true) {
+        opIndex = operandIndex();
+       // console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
+    } else if (displayScreen.innerText !== "" && numberAfterOperation == true) {
         numberAfterOperation = true;
         secondOperand = displayScreen.innerText.substring(opIndex + 1);
         opIndex = null;
         displayScreen.innerText = operate(Number(firstOperand), operation, Number(secondOperand));
-        if (!displayScreen.innerText == "") {
+        if (displayScreen.innerText !== "") {
             firstOperand = displayScreen.innerText;
             if (firstOperand.includes(".")) {
                 isDot = true;
             }
             secondOperand = null;
             operation = null;
-            console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
+            //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
         }
 
-        console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
+        //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
     }
 
-});
-
-deleteBTN.addEventListener("click", () => {
-    if (!displayScreen.innerText == "" && numberAfterOperation == true) {
-        displayScreen.innerText += deleteBTN.textContent;
-    }
 });
 
 dot.addEventListener("click", () => {
-    if (!displayScreen.innerText == "" && !displayScreen.innerText.endsWith("+") && !displayScreen.innerText.endsWith("-") && !displayScreen.innerText.endsWith("/") && !displayScreen.innerText.endsWith("*") && isDot == false) {
+    if (displayScreen.innerText !== "" && !displayScreen.innerText.endsWith("+") && !displayScreen.innerText.endsWith("-") && !displayScreen.innerText.endsWith("/") && !displayScreen.innerText.endsWith("*") && isDot == false) {
         displayScreen.innerText += dot.textContent;
         isDot = true;
     }
@@ -159,9 +197,9 @@ function createCalc() {
         button.innerText = operands[i];
         operationsDIV.appendChild(button);
         button.addEventListener("click", () => {
-            if (!displayScreen.innerText == "" && numberAfterOperation == true && opIndex == null) {
-                if(displayScreen.innerText.endsWith(".")){
-                    displayScreen.innerText+="0";
+            if (displayScreen.innerText !== "" && numberAfterOperation == true && opIndex == null) {
+                if (displayScreen.innerText.endsWith(".")) {
+                    displayScreen.innerText += "0";
                 }
                 numberAfterOperation = false;
                 firstOperand = displayScreen.innerText;
@@ -169,12 +207,12 @@ function createCalc() {
                 displayScreen.innerText += button.textContent;
                 opIndex = operandIndex();
                 isDot = false;
-                console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
-            } else if (!displayScreen.innerText == "" && numberAfterOperation == true) {
+                //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand} \n${opIndex}`);
+            } else if (displayScreen.innerText !== "" && numberAfterOperation == true) {
                 numberAfterOperation = false;
                 secondOperand = displayScreen.innerText.substring(opIndex + 1);
                 displayScreen.innerText = operate(Number(firstOperand), operation, Number(secondOperand));
-                if (!displayScreen.innerText == "") {
+                if (displayScreen.innerText !== "") {
                     firstOperand = displayScreen.innerText;
                     if (!displayScreen.innerText.includes(".")) {
                         isDot = false;
@@ -182,17 +220,19 @@ function createCalc() {
                     secondOperand = null;
                     operation = button.textContent;
                     displayScreen.innerText += button.textContent;
-                    opIndex = operandIndex();
+                    opIndex = operandIndex();                
 
-
-                }
-
-                console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
+                //console.log(`First: ${firstOperand}\nOperation: ${operation}\nSecond: ${secondOperand}`);
             }
+        }else if(displayScreen.innerText !== "" && numberAfterOperation == false){
+            displayScreen.innerText = displayScreen.innerText.substring(0, displayScreen.innerText.length-1);
+            displayScreen.innerText+=button.textContent;
+            operation = button.textContent;
+        }
 
         });
     }
 }
 
-createCalc();
+
 
